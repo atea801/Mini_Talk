6a020bf271a08f2fe2c64637e8f66b23af9026f7
aautret <aautret@student.42.fr>
2025-08-20 20:37:04 +0200

    client : -  check_pid - send_bit - send_bytes - sen_u32_be

--- client.c
+++ client.c
@@ -6,7 +6,24 @@
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 14:15:24 by aautret           #+#    #+#             */
-/*   Updated: 2025/08/20 14:15:26 by aautret          ###   ########.fr       */
+/*   Updated: 2025/08/20 19:34:06 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+int	main(int ac, char **av)
+{
+	__pid_t	pid;
+	char	*message;
+
+	if (ac != 3)
+	{
+		write(1, "parametre(s) incorrect(s)", 25);
+		exit (0);
+	}
+	pid = (__pid_t)ft_atoi(av[1]);
+	check_pid(pid);
+	message = av[2];
+
+}
---
+++ client_utils.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 19:33:46 by aautret           #+#    #+#             */
+/*   Updated: 2025/08/20 20:02:09 by aautret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+/**
+ * @brief transforme un bit en un signal envoye au serveur
+ *
+ * @param pid
+ * @param bit
+ */
+void	send_bit(__pid_t pid, int bit)
+{
+	if (bit != 1 && bit != 0)
+		return ;
+	if (bit == 0)
+		kill(pid, SIGUSR1);
+	else if (bit == 1)
+		kill(pid, SIGUSR2);
+	usleep(200);
+}
+
+/**
+ * @brief transforme un caratere en signal
+ *
+ * - appel sent_bit pour envoyer chaque bit au serveur sous forme de signal
+ * @param pid
+ * @param c
+ */
+void	send_byte(__pid_t pid, unsigned char c)
+{
+	int	i;
+	int	bit;
+
+	bit = 0;
+	i = 8;
+	while (i--)
+	{
+		bit = ((c >> i) & 1);
+		send_bit(pid, bit);
+	}
+}
+
+/**
+ * @brief
+ * - n = entier 32 bits non signe
+ * - chaque bx correspond a un des 4 octets qui composent n
+ *  *
+ * @param pid
+ * @param len
+ */
+void	sen_u32_be(__pid_t pid, uint32_t len)
+{
+	uint32_t		n;
+	unsigned char	b3;
+	unsigned char	b2;
+	unsigned char	b1;
+	unsigned char	b0;
+
+	n = len;
+	b3 = (n >> 24) & 0xFF;
+	b2 = (n >> 16) & 0xFF;
+	b1 = (n >> 8) & 0xFF;
+	b0 = (n >> 0) & 0xFF;
+
+	send_byte(pid, b3);
+	send_byte(pid, b2);
+	send_byte(pid, b1);
+	send_byte(pid, b0);
+}
+
+/**
+ * @brief
+ * - condition 1 : verifie si le pid est pas interdit (-1)
+ * ou autre comportement (0)
+ *
+ * - condition 2 : verifie si on a les autorisations d'y envoyer un signal
+ * - ESRCH => le PID n'existe pas
+ *
+ * = test d'accessibilite et d'exitance du rpocesseur serveur
+ *
+ *
+ * @param pid
+ */
+void	check_pid(__pid_t pid)
+{
+	if (pid <= 0)
+	{
+		ft_printf("Veuillez choisir un autre PID !\n");
+		exit (1);
+	}
+	if (kill(pid, 0) == -1)
+	{
+		if (errno == ESRCH)
+		{
+			write(2, "Erreur: PID inexistant\n", 23);
+			exit(1);
+		}
+	}
+}
