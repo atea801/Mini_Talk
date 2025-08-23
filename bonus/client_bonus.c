e8ddbfa581c759bf5de718c32fefc9d2e2a0fb4d
aautret <aautret@student.42.fr>
2025-08-23 16:24:21 +0200

    bonus : client main

--- bonus/client_bonus.c
+++ bonus/client_bonus.c
@@ -6,8 +6,27 @@
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 14:15:21 by aautret           #+#    #+#             */
-/*   Updated: 2025/08/20 14:32:52 by aautret          ###   ########.fr       */
+/*   Updated: 2025/08/23 16:05:28 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+int	main(int ac, char **av)
+{
+	pid_t	pid;
+	char	*message;
+
+	if (ac != 3)
+	{
+		write(1, "parametre(s) incorrect(s)", 25);
+		exit (0);
+	}
+	pid = (pid_t)ft_atoi(av[1]);
+	check_pid(pid);
+	message = av[2];
+
+	ft_printf("Envoi en cours...\n");
+	send_message(pid, message);
+	ft_printf("Message envoyé!\n");
+}
