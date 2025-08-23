8126e6c13542872d4cac5daff26ac579d733389a
aautret <aautret@student.42.fr>
2025-08-23 16:24:42 +0200

    bonus : serveur bonnus

---
+++ bonus/serveur_bonus.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   serveur_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 14:15:38 by aautret           #+#    #+#             */
+/*   Updated: 2025/08/23 16:06:47 by aautret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+
+// Variable globale - persiste entre les appels du handler
+static t_reception	g_srv;
+
+/**
+ * @brief Handler avancé avec accusé de réception
+ * Convertit les signaux en bits et envoie une confirmation au client
+ */
+void	advanced_handler(int signo, siginfo_t *info, void *ucontext)
+{
+	int		bit;
+	pid_t	client_pid;
+
+	(void)ucontext;
+	client_pid = info->si_pid;
+	g_srv.client_pid = client_pid;
+	if (signo == SIGUSR1)
+		bit = 0;
+	else if (signo == SIGUSR2)
+		bit = 1;
+	else
+		return ;
+	if (g_srv.phase == 0)
+		handle_size_bit(&g_srv, bit);
+	else if (g_srv.phase == 1)
+		handle_content_bit(&g_srv, bit);
+	kill(client_pid, SIGUSR1);
+}
+
+/**
+ * @brief Set the sigactions object
+ *
+ * - quand on recois le signal SIG1 ou SIG2 on appel sig_handler
+ *
+ * - c'est "la colle entre le monde des signaux et le code cote serveur"
+ *
+ */
+void	set_sigactions(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_sigaction = advanced_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		_exit(1);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		_exit(1);
+}
+
+int	main(void)
+{
+	ft_printf("Serveur PID: %d\n", getpid());
+	reset_state(&g_srv);
+	set_sigactions();
+	while (1)
+		pause();
+
+	return (0);
+}
