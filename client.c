5015cac39965df127cfbc404a3ded31644c619b6
aautret <aautret@student.42.fr>
2025-08-23 15:16:48 +0200

    client : printf d'etat "envoie en cours" "message envoye"

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 14:15:24 by aautret           #+#    #+#             */
-/*   Updated: 2025/08/21 19:47:04 by aautret          ###   ########.fr       */
+/*   Updated: 2025/08/23 15:07:58 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,8 @@ int	main(int ac, char **av)
 	pid = (pid_t)ft_atoi(av[1]);
 	check_pid(pid);
 	message = av[2];
+
+	ft_printf("Envoi en cours...\n");
 	send_message(pid, message);
+	ft_printf("Message envoyé!\n");
 }
