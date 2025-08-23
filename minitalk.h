7c508c43d2e260d05eb2d9aa954d69fb015a38fa
aautret <aautret@student.42.fr>
2025-08-23 15:20:54 +0200

    basic : add - definition posix struc sigaction - changement ordre inlcude signal.h & types.h - inlcude structure de reception de serveur - suppression variable globale externe - proto cote serveur

--- minitalk.h
+++ minitalk.h
@@ -6,25 +6,42 @@
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 14:15:28 by aautret           #+#    #+#             */
-/*   Updated: 2025/08/21 19:57:41 by aautret          ###   ########.fr       */
+/*   Updated: 2025/08/23 15:07:58 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
+/* Définitions POSIX - OBLIGATOIRES avant les includes pour struct sigaction complet */
+# define _POSIX_C_SOURCE 200809L
+# define _GNU_SOURCE
+
 # include "ft_printf.h"
 # include "libft.h"
-# include <sys/types.h>   // pid_t
 # include <signal.h>      // struct sigaction, sigaction, sigemptyset, SA_*
+# include <sys/types.h>   // pid_t
 # include <unistd.h>      // write, pause, getpid, usleep
 # include <stdint.h>      // uint32_t
 # include <stdlib.h>      // exit
 # include <errno.h>       // errno, ESRCH
+# include <string.h>      // memset pour ft_bzero
+
+/**
+ * @brief Structure de réception pour le serveur
+ */
+typedef struct s_reception {
+	char			*buffer;
+	uint32_t		expected_size;
+	uint32_t		expected_bits;
+	int				phase;
+	unsigned char	current_byte;
+	uint32_t		index;
+	unsigned char	bit_index;
+}	t_reception;
 
 /**
  * @brief État global : 1 globale par programme (regroupe tout dans une struct
- *
  */
 typedef struct s_srv
 {
@@ -32,18 +49,21 @@ typedef struct s_srv
 	volatile sig_atomic_t	got_usr2;
 }	t_srv;
 
-/**
- * @brief Déclaration (pas définition) de la globale du serveur
- *
- */
-extern t_srv	g_srv;
 
-void	handler(int signum);
-
+//CLIENT
 void	send_bit(pid_t pid, int bit);
 void	send_byte(pid_t pid, unsigned char c);
 void	sen_u32_be(pid_t pid, uint32_t len);
 void	send_message(pid_t pid, const char *s);
 void	check_pid(pid_t pid);
+
+//SERVEUR
+void	simple_handler(int signo);
+void	set_sigactions(void);
+
+void	reset_state(t_reception *rx);
+void	handle_size_bit(t_reception *rx, int bit);
+void	flush_byte_if_ready(t_reception *rx);
+void	handle_content_bit(t_reception *rx, int bit);
 
 #endif
